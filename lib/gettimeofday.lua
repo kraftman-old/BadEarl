ced70f50792c11787735058280f7d392ed8966ff
Chris Tanner <me@itschr.is>
2018-03-24 21:50:47 -0700

    add speed tests for set/sadd/hset, add bloom filter module to redis

--- lib/gettimeofday.lua
+++ lib/gettimeofday.lua
@@ -1,20 +1,21 @@
 -- simple wrapper to get a more precise timer in lua
+-- from https://www.cryptobells.com/openresty-time-resolution-with-ffi-a-more-accurate-approach/
 local ffi = require("ffi")
-
 ffi.cdef[[
-  typedef long time_t;
-  typedef struct timeval {
-    time_t tv_sec;
-    time_t tv_usec;
-  } timeval;
-
-  int gettimeofday(struct timeval* t, void* tzp);
+	typedef long time_t;
+ 
+ 	typedef struct timeval {
+		time_t tv_sec;
+		time_t tv_usec;
+	} timeval;
+ 
+	int gettimeofday(struct timeval* t, void* tzp);
 ]]
-
+ 
+local gettimeofday_struct = ffi.new("timeval")
 local function gettimeofday()
-  local t = ffi.new("timeval")
-  ffi.C.gettimeofday(t, nil)
-  return tonumber(t.tv_sec)
+ 	ffi.C.gettimeofday(gettimeofday_struct, nil)
+ 	return tonumber(gettimeofday_struct.tv_sec) * 1000000 + tonumber(gettimeofday_struct.tv_usec)
 end
 
 return gettimeofday
--- test/performance/storagetest.lua
+++ test/performance/storagetest.lua
@@ -2,8 +2,10 @@ local uuid  = require 'uuid'
 local redis = require 'redis'
 local getTimeOfDay = require './lib/gettimeofday'
 
-local client   = redis.connect('127.0.0.1', 6379)
-local response = client:ping() 
+local red = redis.connect('127.0.0.1', 6379)
+red['bf.add']('test')
+
+local badUrl
 
 -- test 
 -- how many we can store in a set and the speed of retrieval, 
@@ -20,9 +22,64 @@ local getString = function(length)
 end
 
 local a = getTimeOfDay()
-for i = 1, 1000 do
-    getString(90)
+
+-- pipeline 10000 at a time to speed things up a bit
+
+local pipelineWrites = function(p)
+    for i = 1, 10000 do
+        badUrl = getString(90)
+        
+        p:set(badUrl, true)
+    end
 end
-local b = getTimeOfDay()
-print('took: ', b-a)
+
+local pipelineWritesSet = function(p)
+    for i = 1, 10000 do
+        badUrl = getString(90)
+        
+        p:sadd('badurls', badUrl)
+    end
+end
+
+local pipelineWritesHset = function(p)
+    for i = 1, 10000 do
+        badUrl = getString(90)
+        key, badUrl = badUrl:sub(1, 2), badUrl:sub(3)
+        p:hset(key, badUrl, true)
+    end
+end
+
+local pipelinedWrites = function()
+    local i = 0
+    while true do
+        i = i + 10000
+        if i % 100000 == 0 then
+            print('processed: ', i, ' keys')
+        end
+        red:pipeline(pipelineWrites)
+    end
+end
+
+local regularWrites = function() 
+    local urls = {}
+    for i = 1, 100000 do
+        table.insert(urls, getString(90))
+    end
+
+    local a = getTimeOfDay()
+    for i = 1, 100000 do
+        red:set(urls[i], true)
+        --red:sadd('badurls', urls[i])
+        --key, badUrl = badUrl:sub(1, 2), badUrl:sub(3)
+        --red:hset('badurls', urls[i], true)
+    end
+    local b = getTimeOfDay()
+
+    print('took: ', (b-a)/1000, 'ms')
+
+end
+
+regularWrites()
+
+
 
