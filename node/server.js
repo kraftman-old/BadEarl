f41127038534932a62c9e8e968acb60a3bafc9ca
Chris Tanner <me@itschr.is>
2018-03-25 20:59:39 -0700

    redis connection working, very basic url checking working

--- node/server.js
+++ node/server.js
@@ -1,6 +1,51 @@
-const express = require('express')
-const app = express()
+const express  = require('express');
+const bluebird = require('bluebird');
+const redis    = require('redis');
 
-app.get('/', (req, res) => res.send('Working!'))
+bluebird.promisifyAll(redis.RedisClient.prototype);
+bluebird.promisifyAll(redis.Multi.prototype);
+
+const app = express();
+
+
+const red = redis.createClient(process.env.REDIS_PORT, process.env.REDIS_HOST);
+ 
+const validateParams = function(req, res, next) {
+    const badURL = req.query.url;
+
+    if (!badURL) {
+        res.status(400);
+        res.send('please specify a URL!');
+        return next('no url passed');
+    }
+    return next()
+}
+
+const checkURL = function(req, res, next) {
+    const badURL = req.query.url;
+
+    red.sismemberAsync('badUrls', badURL)
+    .then(result => {
+        console.log(result)
+
+        if (result === 0) {
+            return res.send('url is safe!')
+        } else if (result === 1) {
+            return res.send('url is unsafe!')
+        } else {
+            console.log('unknown result: ', result)
+            res.send('unkown error occured')
+            return Promise.reject('unknown error')
+        }
+    })
+    .catch(err => {
+        console.log('error getting from redis: ', err)
+        next(err);
+    });
+}
+
+app.use(validateParams)
+
+app.get('/safeurl', checkURL)
 
 app.listen(process.env.HTTP_PORT, () => console.log('Up!'))
